9cd361fc3990673dbf4217f50e6375d0f69b5fbe
Ahmed Osama <eng.ahmed.osama.1190@gmail.com>
2025-10-10 18:24:59 +0000

    fix: make rate field read-only in edit item dialog

--- POS/src/components/sale/EditItemDialog.vue
+++ POS/src/components/sale/EditItemDialog.vue
@@ -90,8 +90,8 @@
 									type="number"
 									min="0"
 									step="0.01"
-									class="w-full h-10 border border-gray-300 rounded-lg pl-16 pr-3 text-sm font-semibold focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-									@input="calculateTotals"
+									readonly
+									class="w-full h-10 border border-gray-300 rounded-lg pl-16 pr-3 text-sm font-semibold bg-gray-50 cursor-not-allowed"
 								/>
 							</div>
 						</div>
