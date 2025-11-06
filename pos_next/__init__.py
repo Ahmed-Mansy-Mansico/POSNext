49a8c24394bb380fbc60c3f30d93e41074024a9d
engahmed1190 <eng.ahmed.osama.1190@gmail.com>
2025-11-06 15:45:07 +0200

    chore: bump version to 1.4.0

--- pos_next/__init__.py
+++ pos_next/__init__.py
@@ -6,7 +6,7 @@ try:
 except ModuleNotFoundError:  # pragma: no cover - frappe may not be installed during setup
     frappe = None
 
-__version__ = "1.3.0"
+__version__ = "1.4.0"
 
 
 def console(*data):
