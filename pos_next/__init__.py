a1d3a86e09a6e0617408828c9dc89b6929965367
Ahmed Osama <eng.ahmed.osama.1190@gmail.com>
2025-11-08 07:51:11 +0000

    chore: bump version to 1.6.1

--- pos_next/__init__.py
+++ pos_next/__init__.py
@@ -6,7 +6,7 @@ try:
 except ModuleNotFoundError:  # pragma: no cover - frappe may not be installed during setup
     frappe = None
 
-__version__ = "1.6.0"
+__version__ = "1.6.1"
 
 
 def console(*data):
